9dee250536f57d9056087bb78a329fad201e90f4
Cao Anh Quan <aquancva@gmail.com>
2017-08-17 21:49:04 +0700

    change env to https

--- src/constants/env.js
+++ src/constants/env.js
@@ -7,14 +7,14 @@ if (process.env.NODE_ENV === 'development'){
     URL = PROD_URL;
 }
 
-export const API_URL = "http://api."+URL;
-export const MANAGE_API_URL = "http://manageapi."+URL;
-export const BASE_URL = "http://"+URL;
+export const API_URL = "https://api."+URL;
+export const MANAGE_API_URL = "https://manageapi."+URL;
+export const BASE_URL = "https://"+URL;
 
 export const NAME_COMPANY = "KEETOOL";
-export const LINK_LOGO_LIGHT = "http://keetool.s3.amazonaws.com/logo/Artboard+5+copy.png";
-export const LINK_LOGO_DARK = "http://keetool.s3.amazonaws.com/logo/Artboard+5.png";
-export const LINK_LOGO_LONG = "http://keetool.s3.amazonaws.com/logo/logo_long.png";
+export const LINK_LOGO_LIGHT = "https://d255zuevr6tr8p.cloudfront.net/logo/Artboard+5+copy.png";
+export const LINK_LOGO_DARK = "https://d255zuevr6tr8p.cloudfront.net/logo/Artboard+5.png";
+export const LINK_LOGO_LONG = "https://d255zuevr6tr8p.cloudfront.net/logo/logo_long.png";
 export const SECRET_TOKEN = "KEEeducation";
 export const EXPIRES_IN = "6d";
 export const NAME_DATA_LOGIN_SAVE_LOCAL = "datatoken";
